70f9e85406b15e5d90a1accb3997876e5bc1619f
wfercanas <fernando.canas@outlook.com>
2022-02-01 14:26:58 -0500

    add/ client type and methods read() and handle()

---
+++ client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+)
+
+type client struct {
+	conn       net.Conn
+	outbound   chan<- command
+	register   chan<- *client
+	deregister chan<- *client
+	username   string
+}
+
+func (c *client) read() error {
+	for {
+		msg, err := bufio.NewReader(c.conn).ReadBytes('\n')
+		if err == io.EOF {
+			// Coinnection closed, deregister client
+			c.deregister <- c
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		c.handle(msg)
+	}
+}
+
+func (c *client) handle(message []byte) {
+	cmd := bytes.ToUpper(bytes.TrimSpace(bytes.Split(message, []byte(" "))[0]))
+	args := bytes.TrimSpace(bytes.TrimPrefix(message, cmd))
+
+	switch string(cmd) {
+	case "REG":
+		if err := c.reg(args); err != nil {
+			c.err(err)
+		}
+	case "JOIN":
+		if err := c.join(args); err != nil {
+			c.err(err)
+		}
+	case "LEAVE":
+		if err := c.leave(args); err != nil {
+			c.err(err)
+		}
+	case "MSG":
+		if err := c.msg(args); err != nil {
+			c.err(err)
+		}
+	case "CHNS":
+		c.chns()
+	case "USRS":
+		c.usrs()
+	default:
+		c.err(fmt.Errorf("Unknow command %s", cmd))
+	}
+}

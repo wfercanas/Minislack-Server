ab4140cb4f914e8e20a47afc72417d52398f641b
wfercanas <fernando.canas@outlook.com>
2022-02-01 15:23:21 -0500

    add/ run() method

--- hub.go
+++ hub.go
@@ -1,9 +1,37 @@
 package main
 
+import "fmt"
+
 type hub struct {
 	channels        map[string]*channel
 	clients         map[string]*client
 	commands        chan command
 	deregistrations chan *client
 	registrations   chan *client
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case client := <-h.registrations:
+			h.register(client)
+		case client := <-h.deregistrations:
+			h.unregister(client)
+		case cmd := <-h.commands:
+			switch cmd.id {
+			case JOIN:
+				h.joinChannel(cmd.sender, cmd.recipient)
+			case LEAVE:
+				h.leaveChannel(cmd.sender, cmd.recipient)
+			case MSG:
+				h.message(cmd.sender, cmd.recipient, cmd.body)
+			case USRS:
+				h.listUsers(cmd.sender)
+			case CHNS:
+				h.listChannels(cmd.sender)
+			default:
+				fmt.Errorf("Hub Switch: Cannot process %s", cmd.id)
+			}
+		}
+	}
 }

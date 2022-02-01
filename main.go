8941162f0ba767bb5d079660eb0aa7a6617b8898
wfercanas <fernando.canas@outlook.com>
2022-02-01 17:38:27 -0500

    add/ main() func

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	hub := newHub()
+	go hub.run()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("%v", err)
+		}
+
+		c := newClient(
+			conn,
+			hub.commands,
+			hub.registrations,
+			hub.deregistrations,
+		)
+
+		go c.read()
+	}
+}

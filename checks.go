e24226512b1e720a45d78d013150de07b937e670
wfercanas <fernando.canas@outlook.com>
2022-02-16 20:07:56 -0500

    add/ check if a user is a member of a channel

--- checks.go
+++ checks.go
@@ -9,3 +9,8 @@ func (h *hub) channelExists(ch string) bool {
 	_, exists := h.channels[ch]
 	return exists
 }
+
+func (h *hub) userIsMember(ch *channel, cl *client) bool {
+	_, exists := ch.clients[cl]
+	return exists
+}

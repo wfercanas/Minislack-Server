ba1ed5f7f6df7b026b17a306da0b8328ad23b8e1
wfercanas <fernando.canas@outlook.com>
2022-02-16 19:23:56 -0500

    add/ check a channel exists

--- checks.go
+++ checks.go
@@ -4,3 +4,8 @@ func (h *hub) userRegistered(username string) bool {
 	_, exists := h.clients[username]
 	return exists
 }
+
+func (h *hub) channelExists(ch string) bool {
+	_, exists := h.channels[ch]
+	return exists
+}

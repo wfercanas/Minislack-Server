ed5ca0ec6fd5f7bf8fad7248617c72779f399c52
wfercanas <fernando.canas@outlook.com>
2022-02-17 19:09:19 -0500

    add/ get uses commChannelDoesntExist

--- hub.go
+++ hub.go
@@ -247,8 +247,7 @@ func (h *hub) getFile(cl *client, ch string, filename []byte) {
 	sender := h.clients[cl.username]
 
 	if !h.channelExists(ch) {
-		response = fmt.Sprintf("GET Failed: channel %s doesn't exist\n", ch)
-		communicate(response, cl.conn)
+		commChannelDoesntExist("GET", ch, cl.conn)
 		return
 	}
 	channel := h.channels[ch]
